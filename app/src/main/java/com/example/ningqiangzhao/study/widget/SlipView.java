ff1f88e8d3230d7e1c195c203c906a6b66a80335
zhaoningqiang <zhaoningqiang@ujipin.cn>
2016-06-04 17:20:58 +0800

    添加完全

---
+++ app/src/main/java/com/example/ningqiangzhao/study/widget/SlipView.java
@@ -0,0 +1,23 @@
+package com.example.ningqiangzhao.study.widget;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * @description: author:zhaoningqiang
+ * @time 16/6/2/下午6:05
+ */
+public class SlipView extends RecyclerView {
+    public SlipView(Context context) {
+        super(context);
+    }
+
+    public SlipView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlipView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
